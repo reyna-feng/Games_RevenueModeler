34a7d2a5b1c3ca8e836337223372cf8c156ed31a
reyna-feng <reyna.feng@unity3d.com>
2022-03-25 10:05:36 -0500

    add login and actual revenue

--- actual_revenue.R
+++ actual_revenue.R
@@ -8,22 +8,25 @@ sale_data<-reactive({
   sale_data <- dbGetQuery(vertica, sale_sql)
   sale_data <- as.data.frame(sale_data)
   sale_data$date<-as.Date(sale_data$date,format="%Y-%m-%d")
-  sale_data<-sale_data %>% arrange(date)
+  sale_data<-sale_data %>% 
+    dplyr::arrange(date)
   return(sale_data)
 })
 
 output$sale_actual_diff<-renderPlot({
   sale_data<-sale_data()
   final_data<-calculation_2() %>%
-    select(date,platform,daily_ini_revenue)%>%
-    distinct()%>%
-    group_by(date) %>%
-    mutate(total_iap_revenue=sum(daily_ini_revenue)) %>%
-    select(date,total_iap_revenue) %>%
-    distinct() %>% arrange(date)
-  sale_data_join<-merge(x=sale_data,y=final_data,by=c("date"),all.x=TRUE) %>%
-    mutate(revenue_diff=revenue-total_iap_revenue) %>%
-    arrange(date)
+    dplyr::select(date,platform,daily_ini_revenue)%>%
+    dplyr::distinct()%>%
+    ggpubr::group_by(date) %>%
+    ggpubr::mutate(total_iap_revenue=sum(daily_ini_revenue)) %>%
+    dplyr::select(date,total_iap_revenue) %>%
+    dplyr::distinct() %>% 
+    dplyr::arrange(date)
+  sale_data_join<-
+    merge(x=sale_data,y=final_data,by=c("date"),all.x=TRUE) %>%
+    ggpubr::mutate(revenue_diff=revenue-total_iap_revenue) %>%
+    dplyr::arrange(date)
   ggplot(data=sale_data_join)+
     geom_bar(aes(x=date,y=revenue_diff),stat="identity", fill="skyblue4",color="#006000")+
     labs(title= "Actual Revenue",x="Date",y="Revenue Diff")+
@@ -32,10 +35,10 @@ output$sale_actual_diff<-renderPlot({
 output$sale_actual_plot<-renderPlot({
   sale_data<-sale_data()
   sale_data<-sale_data %>%
-    mutate(yearmonth=format(date,"%Y-%m")) %>%
-    group_by(yearmonth) %>%
-    mutate(monthly_revenue=sum(revenue)) %>%
-    select(yearmonth,monthly_revenue) %>% distinct() %>% arrange(yearmonth)
+    ggpubr::mutate(yearmonth=format(date,"%Y-%m")) %>%
+    ggpubr::group_by(yearmonth) %>%
+    ggpubr::mutate(monthly_revenue=sum(revenue)) %>%
+    dplyr::select(yearmonth,monthly_revenue) %>% distinct() %>% arrange(yearmonth)
   ggplot(data=sale_data)+
     geom_bar(aes(x=yearmonth,y=monthly_revenue),stat="identity", fill="skyblue4",color="#006000")+
     labs(title= "Actual Revenue",x="YearMonth",y="Revenue")+
